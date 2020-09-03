6a350c57142a2eceea154b42c53ae6ab16c00d4c
D <danixoon@gmail.com>
2020-09-03 12:01:39 +0300

    secret url params generator

--- src/index.ts
+++ src/index.ts
@@ -89,24 +89,51 @@ client.on("message", async (msg) => {
         .map(([req, ans], i) => `request_${i}=${req}&answer_${i}=${ans}`)
         .join("&");
 
+      const getRandomArray = () => {
+        let a: number;
+        let arr = [];
+        for (let i = 0; i < 256; i++) {
+          a = i;
+          for (let j = 0; j < 8; j++) {
+            a = a & 0x1 ? 0xedb88320 ^ (a >>> 0x1) : a >>> 0x1;
+          }
+          arr[i] = a;
+        }
+        return arr;
+      };
+
+      const crc = (value: string) => {
+        let arr = getRandomArray();
+        let a = 0x0 ^ -0x1;
+        for (let i = 0; i < value.length; i++) {
+          a = (a >>> 0x8) ^ arr[(a ^ value.charCodeAt(i)) & 0xff];
+        }
+        return (a ^ -0x1) >>> 0x0;
+      };
+
+      const getCRCSign = (time) => {
+        return crc(
+          "public-api" +
+            time +
+            process.env.TALK_KEY1 +
+            process.env.TALK_KEY2 +
+            process.env.TALK_KEY3
+        );
+      };
+
+      const getSecretKey = (time: string) => {
+        return `a=public-api&b=${crc(time + "b")}&c=${getCRCSign(time)}&d=${crc(
+          Date.now() + "d"
+        )}&e=${Math.random()}&t=${time}&x=${Math.random() * 10}`;
+      };
+
+      const secret = getSecretKey(new Date().getTime().toString());
+
       const body = `request=${msg.content}&bot_name=кит&user_name=${
         msg.member.displayName
-      }${dialogRequest ? `&${dialogRequest}` : ""}&${process.env.TALK_URL}`;
+      }${dialogRequest ? `&${dialogRequest}` : ""}&dialog_lang=ru&${secret}`;
 
-      const answer = await axios.post("http://p-bot.ru/api/getAnswer", body, {
-        headers: {
-          // accept: "*/*",
-          // "accept-language": "ru",
-          // "content-type": "application/x-www-form-urlencoded",
-
-          // referrer: "http://p-bot.ru/",
-          // referrerPolicy: "no-referrer-when-downgrade",
-
-          method: "POST",
-          // mode: "cors",
-          // credentials: "include",
-        },
-      });
+      const answer = await axios.post("http://p-bot.ru/api/getAnswer", body);
 
       msg.reply(answer.data.answer);
 
